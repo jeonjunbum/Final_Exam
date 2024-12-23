7a69485655f0cf3453f4763853cbf20d5b3ec1e8
jeonjunbum <sky195153@navr.com>
2024-12-24 03:39:02 +0900

    클래스 상속 되도록 수정

--- src/main/java/Scholarship.java
+++ src/main/java/Scholarship.java
@@ -3,8 +3,7 @@ import javax.swing.*;
 public class Scholarship {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            ScholarshipFrame frame = new ScholarshipFrame();
-            frame.setVisible(true);
+            new ScholarshipFrame(new ScholarshipCalculator(), new InputValidator(), new ScholarshipRequirementsLoader()).setVisible(true);
         });
     }
 }
